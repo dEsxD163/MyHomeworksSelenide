4081261785e0c716f310114778783997efb6464c
Denis <dzavalskiy@gmail.com>
2023-05-26 22:36:16 +0400

    One commit

---
+++ src/test/java/DeliveryCardTest.java
@@ -0,0 +1,58 @@
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DeliveryCardTest {
+    String generateDate (int dayToAdd) {
+        return LocalDate.now().plusDays(dayToAdd).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+    String meetingDate = generateDate(3);
+
+    @Test
+    void DeliveryCardOrderTest1() {
+
+        open("http://localhost:9999");
+        $("[data-test-id=city] input").setValue("Казань");
+        $("[data-test-id=date] input").clear();
+        $("[data-test-id=date] input").setValue(meetingDate);
+        $("[data-test-id=name] input").setValue("Заседателев Дмитрий");
+        $("[data-test-id=phone] input").setValue("+79271234567");
+        $("[data-test-id=agreement]").click();
+        $(withText("Забронировать")).click();
+        $(byText("Успешно!")).shouldBe(visible, Duration.ofMillis(15000));
+    }
+    @Test
+    void DeliveryCardOrderTest2() {
+        open("http://localhost:9999");
+        $("[data-test-id=city] input").setValue("Казань");
+        $("[data-test-id=date] input").clear();
+        $("[data-test-id=date] input").setValue(meetingDate);
+        $("[data-test-id=name] input").setValue("Заседателев Дмитрий");
+        $("[data-test-id=phone] input").setValue("+79271234567");
+        $("[data-test-id=agreement]").click();
+        $(withText("Забронировать")).click();
+        $(".notification__content").shouldBe(visible, Duration.ofSeconds(15));
+    }
+
+    @Test
+    void DeliveryCardOrderTest3() {
+        open("http://localhost:9999");
+        $("[data-test-id=city] input").setValue("Казань");
+        $("[data-test-id=date] input").clear();
+        $("[data-test-id=date] input").setValue(meetingDate);
+        $("[data-test-id=name] input").setValue("Заседателев Дмитрий");
+        $("[data-test-id=phone] input").setValue("+79271234567");
+        $("[data-test-id=agreement]").click();
+        $(withText("Забронировать")).click();
+        $(".notification__content").shouldHave(text("Встреча успешно забронирована на " + meetingDate),
+                Duration.ofSeconds(15)).shouldBe(visible);
+    }
+}
